921e718901cb8524b8d09c43742d0d03f8b837ee
VolatileAce <andrewchristofidisdesign@gmail.com>
2018-04-16 11:34:40 +1000

    Made more folders, made a bad player model to test player script

---
+++ Game Jam #2/Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    //player should spawn above the ground
+    public bool isGrounded = false;
+
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Environment")
+        {
+            isGrounded = true;
+        }
+    }
+    private void OnMouseDown()
+    {
+        if (isGrounded)
+        {
+            Jump();
+        }
+        else if (!isGrounded)
+        {
+            Rotate();
+        }
+    }
+    public void Jump()
+    {
+        Debug.Log("BOING!");
+        isGrounded = false;
+    }
+    public void Rotate()
+    {
+        Debug.Log("WEEEEEEE!");
+    }
+}
