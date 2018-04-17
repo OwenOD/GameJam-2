74d0e0916e265f225c3f4aa36fa9218818c36408
LiamW <liamw98@hotmail.com>
2018-04-17 16:52:32 +1000

    Started Scoring

---
+++ Game Jam 2/Assets/Scripts/Score.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    //public Text scoreText;
+
+    private float currentScoreCount;
+    private float highScore;
+    private float distance;
+
+    [SerializeField] GameObject player;
+    [SerializeField] bool isPlayerAlive = true;
+
+    // Use this for initialization
+    void Start ()
+    {
+        //distance = transform;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        while (isPlayerAlive == true)
+        {
+            currentScoreCount++;
+        }
+	}
+}
